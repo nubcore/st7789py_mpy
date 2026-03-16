e2825cdb6f02692fb94e8ac0d2a25f9720c81398
NUBcore <beau@nubcore.dk>
2026-03-16 16:46:28 +0100

    Metro S3 & XIAO C3 autoconfig pinouts

--- tft_configs/esp32_284x76/tft_config.py
+++ tft_configs/esp32_284x76/tft_config.py
@@ -1,7 +1,7 @@
-"""Generic ESP32 320x240
+"""Generic ESP32 284x76
 
 """
-
+import sys
 from machine import Pin, SoftSPI
 import st7789py as st7789
 
@@ -16,12 +16,23 @@ def config(rotation=1):
         ST7789: An instance of the ST7789 display driver.
     """
 
+    build = sys.implementation._build
+    print(build)
+    if build == "ESP32_GENERIC_C3":
+        # seeeed studio 'XIAO' ESP32-C3
+        print("C3 Config")
+        sck, mosi, miso, reset, cs, dc, backlight = 8, 10, 0, 4, 3, 9, 5
+    else:
+        # Adafruit 'Metro' ESP32-S3
+        print("S3 Config")
+        sck, mosi, miso, reset, cs, dc, backlight = 8, 9, 0, 10, 12, 11, 13
+
     return st7789.ST7789(
-        SoftSPI(baudrate=40000000, sck=Pin(8), mosi=Pin(9), miso=Pin(0)),
+        SoftSPI(baudrate=40000000, sck=Pin(sck), mosi=Pin(mosi), miso=Pin(miso)),
         76,
         284,
-        reset=Pin(10, Pin.OUT),
-        cs=Pin(12, Pin.OUT),
-        dc=Pin(11, Pin.OUT),
-        backlight=Pin(13, Pin.OUT),
+        reset=Pin(reset, Pin.OUT),
+        cs=Pin(cs, Pin.OUT),
+        dc=Pin(dc, Pin.OUT),
+        backlight=Pin(backlight, Pin.OUT),
         rotation=rotation)
